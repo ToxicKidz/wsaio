50a037e093b03bace89928c641469dba9e375b98
asleep-cult <asleep.cult@gmail.com>
2021-09-27 20:42:33 -0400

    message

--- wsaio/exceptions.py
+++ wsaio/exceptions.py
@@ -6,3 +6,6 @@ class InvalidFrameError(InvalidDataError):
     def __init__(self, message, code):
         self.message = message
         self.code = code
+
+    def __str__(self):
+        return self.message
