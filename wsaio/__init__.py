61cc12cf0dd1371d07543504f07151a4a1167666
asleep-cult <asleep.cult@gmail.com>
2021-12-12 14:45:49 -0500

    some imports in the thing

--- wsaio/__init__.py
+++ wsaio/__init__.py
@@ -4,4 +4,18 @@ from .exceptions import (
     InvalidDataError,
     InvalidFrameError,
 )
-from .frame import WebSocketFrame
+from .frame import (
+    WS_ABNORMAL_CLOSURE,
+    WS_GOING_AWAY,
+    WS_INTERNAL_SERVER_ERROR,
+    WS_INVALID_PAYLOAD_DATA,
+    WS_MANDATORY_EXTENSION,
+    WS_MESSAGE_TOO_BIG,
+    WS_NORMAL_CLOSURE,
+    WS_NO_STATUS_RECEIVED,
+    WS_POLICY_VIOLATION,
+    WS_PROTOCOL_ERROR,
+    WS_TLS_HANDSHAKE,
+    WS_UNSUPPORTED_DATA,
+    WebSocketFrame
+)
